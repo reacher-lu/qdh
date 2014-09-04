8e502402262fccae1d397b662d8e1ac33b5be224
Reacher <372142160@qq.com>
2014-09-04 14:22:47 +0800

    hotel commit

--- app/scripts/qdh/hotel_comment.js
+++ app/scripts/qdh/hotel_comment.js
@@ -1,9 +1,17 @@
 var hotelComment = {
   init : function(){
-    console.log('hotel');
+    this.process();
+  },
+
+  process : function(){
+    $("[data-process]").each(function(){
+      var score = parseFloat($(this).closest("li").find(".scores").text(),10)*2*10+"%";
+      // console.log(score);
+      $(this).find("i").animate({
+        "width":score
+      },1500);
+    });
   }
-
-
 
 };
 
